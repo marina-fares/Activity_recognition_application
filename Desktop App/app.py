2b16d6c760a5f1f3b85d940e31b89a582e9f5505
amirrami <amir.r.zarif@hotmail.com>
2020-05-24 22:17:46 +0200

    add kinetics_i3d model

---
+++ Desktop App/app.py
@@ -0,0 +1,55 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QLineEdit,QFileDialog,QMainWindow,QApplication
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtCore import pyqtSlot
+import icon_rc
+import sys
+
+from kinetics_i3d_master.preprocessing import preprocessing
+from kinetics_i3d_master.evaluate_sample import activity_recogniton
+
+class Ui(QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('design3.ui', self)
+        self.show()
+        self.handel_buttons()
+        self.file_name = None
+
+    def handel_buttons(self):
+        self.start_btn.clicked.connect(self.start_processing)
+        self.browse_btn.clicked.connect(self.browse_video)
+
+    def start_processing(self):
+        print("Done")
+        print(self.comboBox.currentText())
+        if(self.file_name):
+            numpy_frames = preprocessing(self.file_name)
+            activity_recogniton(self.file_name,numpy_frames)
+
+
+    def browse_video(self):
+        print("Done")
+        options = QFileDialog.Options()
+        #options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Input Video", "","Videos (*.mp4 *.avi)", options=options)
+        if fileName:
+            print(fileName)
+            self.file_name = fileName
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
+
+
+''' 
+buttons' names ::
+1. browse_btn
+2. start_btn
+3. home_btn
+4. video_btn
+5. history_btn
+'''
