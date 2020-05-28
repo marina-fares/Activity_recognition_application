170c68d657ca93e859ea6ce4eb53c1ccf0dd5442
marina-fares <marina.fares11@gmail.com>
2020-05-28 13:54:14 +0000

    add main.ui

--- Desktop App/app.py
+++ Desktop App/app.py
@@ -11,17 +11,73 @@ import os
 from kinetics_i3d_master.preprocessing import preprocessing
 from kinetics_i3d_master.evaluate_sample import activity_recogniton
 
+from PyQt5.QtWidgets import QApplication, QFileDialog, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget, QStatusBar
+
 class Ui(QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
-        uic.loadUi('design3.ui', self)
+        uic.loadUi('main.ui', self)
         self.show()
+		self.start_video()
         self.handel_buttons()
         self.file_name = None
         self.output_path = None
         self.first_run = True
         self.working_dir = os.getcwd()
         self.main_output_folder_structure = None
+
+    def start_video(self):
+		
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        videoWidget = QVideoWidget()
+        videoWidget.setFixedHeight(250)
+        self.play_btn.setEnabled(False)
+        self.play_btn.setFixedHeight(24)
+        self.play_btn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.play_btn.clicked.connect(self.play)
+        self.h_slider.setRange(0, 0)
+        self.h_slider.sliderMoved.connect(self.setPosition)
+        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
+        self.verticalLayout.addWidget(videoWidget,1)
+        self.frame_2.setLayout(self.verticalLayout)
+        self.abrir()
+		
+    def abrir(self):
+        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("test_video.mp4")))
+        self.play_btn.setEnabled(True)
+        self.play()
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+        
+    def positionChanged(self, position):
+        self.h_slider.setValue(position)
+
+    def durationChanged(self, duration):
+	    self.h_slider.setRange(0, duration)
+
+    def setPosition(self, position):
+	    self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+	    self.play_btn.setEnabled(False)
+	    self.statusbar.showMessage("Error: " + self.mediaPlayer.errorString())
+
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.play_btn.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.play_btn.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
         
 
     def handel_buttons(self):
