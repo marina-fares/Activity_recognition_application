3d8f221f7d00357a904a8388e78d333f604d2085
marina-fares <marina.fares11@gmail.com>
2020-05-29 01:50:23 +0200

    add tree graph

--- Desktop App/app.py
+++ Desktop App/app.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QLineEdit,QFileDialog,QMainWindow,QApplication
+from PyQt5.QtWidgets import QLineEdit,QFileDialog,QMainWindow,QApplication, QFileSystemModel
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtCore import pyqtSlot
@@ -14,7 +14,7 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from kinetics_i3d_master.preprocessing import preprocessing
 from kinetics_i3d_master.evaluate_sample import activity_recogniton
 
-from PyQt5.QtWidgets import QApplication, QFileDialog, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget, QStatusBar
+from PyQt5.QtWidgets import QApplication, QFileDialog, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget, QStatusBar, QTreeView
 
 class Ui(QMainWindow):
     def __init__(self):
@@ -30,10 +30,40 @@ class Ui(QMainWindow):
         self.working_dir = os.getcwd()
         self.main_output_folder_structure = None
 
+        
+
+    def draw(self, main_folder_path):        
+        if self.first_run :
+            dirpath = main_folder_path
+            self.model = QFileSystemModel()
+            self.model.setRootPath(dirpath)
+            self.tree =  QTreeView()
+            self.tree.setModel(self.model)
+            self.tree.setRootIndex(self.model.index(dirpath))
+            self.verticalLayout.addWidget(self.tree)
+            self.frame_2.setLayout(self.verticalLayout)
+            self.tree.doubleClicked.connect(self.test)
+            self.first_run = False
+        else :
+            dirpath = main_folder_path            
+            self.model.setRootPath(dirpath)
+            self.tree.setModel(self.model)
+            self.tree.setRootIndex(self.model.index(dirpath))
+            
+   
+    def test(self, signal):
+        file_path=self.model.filePath(signal)
+        self.abrir(file_path)
+        print(file_path)
+
+    
+        
+
     def start_video(self):	
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         videoWidget = QVideoWidget()
         videoWidget.setFixedHeight(250)
+        videoWidget.setFixedWidth(500)
         self.play_btn.setEnabled(False)
         self.play_btn.setFixedHeight(24)
         self.play_btn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
@@ -47,10 +77,10 @@ class Ui(QMainWindow):
 
         self.verticalLayout.addWidget(videoWidget,1)
         self.frame_2.setLayout(self.verticalLayout)
-        self.abrir()
+        self.abrir("/home/marina/Graduation project/kinetics-marina/GP_design/Desktop App/test_video.mp4")
 		
-    def abrir(self):
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("/home/marina/Graduation project/kinetics-marina/GP_design/Desktop App/test_video.mp4")))
+    def abrir(self,path):
+        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(path)))
         self.play_btn.setEnabled(True)
         self.play()
 
@@ -80,7 +110,7 @@ class Ui(QMainWindow):
                     self.style().standardIcon(QStyle.SP_MediaPause))
         else:
             self.play_btn.setIcon(
-                    self.style().standardIcon(QStyle.SP_MediaPlay))
+            self.style().standardIcon(QStyle.SP_MediaPlay))
         
 
     def handel_buttons(self):
@@ -88,7 +118,6 @@ class Ui(QMainWindow):
         self.browse_btn_2.clicked.connect(self.browse_video)
 
     def start_processing(self):
-        print("Done")
         print(self.comboBox.currentText())
         if(self.file_name):
             os.chdir(self.working_dir)
@@ -96,15 +125,9 @@ class Ui(QMainWindow):
             if not self.output_path:
                 self.output_path = os.path.dirname(os.path.abspath(self.file_name))
             self.main_output_folder_structure = activity_recogniton(self.file_name,numpy_frames,self.output_path,self.first_run)
-            print("\n\n\n\n")
-            if len(self.main_output_folder_structure.sub_folder_list)>0:
-                print(self.main_output_folder_structure.sub_folder_list[0].path)
-            else:
-                print("no sub folders")
-            print(self.main_output_folder_structure.path)
-            print(self.main_output_folder_structure.main_video)
-            print("\n\n\n\n")
-            self.first_run = False
+            
+            self.draw( self.main_output_folder_structure.path)
+            self.tabWidget.setCurrentIndex(1)
             
             
 
@@ -113,15 +136,11 @@ class Ui(QMainWindow):
         #options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self,"Input Video", "","Videos (*.mp4 *.avi)", options=options)
         if fileName:
-            print(fileName)
+            self.lineEdit_2.setText(fileName)
+            #print(fileName)
             self.file_name = fileName
-            #print(os.path.dirname(os.path.abspath(fileName)))
-            #print(os.getcwd())
-            #os.chdir(os.path.dirname(os.path.abspath(fileName)))
-            #print(os.getcwd())
-            #dir_name = "test"
-            #os.mkdir(dir_name)
-
+        else:
+            self.file_name = self.lineEdit_2.text()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
